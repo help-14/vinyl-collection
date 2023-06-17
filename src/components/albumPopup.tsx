e1203258b3628f0bcdbcde3912b0deaff22c8598
Help-14 <help-14@hotmail.com>
2023-06-17 17:11:25 +0700

    fix: popup content

--- src/components/albumPopup.tsx
+++ src/components/albumPopup.tsx
@@ -41,16 +41,7 @@ const AlbumPopup: Component = (props) => {
           </div>
           <div class="p-6 space-y-6">
             <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new
-              consumer privacy laws for its citizens, companies around the world
-              are updating their terms of service agreements to comply.
-            </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.)
-              goes into effect on May 25 and is meant to ensure a common set of
-              data rights in the European Union. It requires organizations to
-              notify users as soon as possible of high-risk data breaches that
-              could personally affect them.
+              Feature is comming soon!
             </p>
           </div>
           <div class="flex items-center p-6 space-x-2 border-gray-200 rounded-b dark:border-gray-600"></div>
