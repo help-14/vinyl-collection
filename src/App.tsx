77534d8073e9abdf1be9f791571b037b95eb62fd
Help-14 <help-14@hotmail.com>
2023-06-22 23:02:33 +0700

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -79,6 +79,20 @@ const App: Component = () => {
       >
         <AlbumPopup info={selected()} />
       </div>
+
+      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" - "}
+            <a href="https://nhan.pt/" class="hover:underline">
+              NhanPT
+            </a>
+          </span>
+          <p class="items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            {albums.length} albums
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
