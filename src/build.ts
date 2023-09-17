b344bf72dafddbb0c7a5bca6c644818ee7e95c3f
Help-14 <help-14@hotmail.com>
2023-09-17 17:39:39 +0700

    fix build script

---
+++ src/build.ts
@@ -0,0 +1,46 @@
+import { Albums } from "./data/albums"
+import * as fs from "fs"
+import * as https from "https"
+
+async function downloadFile(url: string, targetFile: string) {
+  console.log(`Download from ${url} to ${targetFile}`)
+  return await new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        const code = response.statusCode ?? 0
+
+        if (code >= 400) {
+          return reject(new Error(response.statusMessage))
+        }
+
+        // handle redirects
+        if (code > 300 && code < 400 && !!response.headers.location) {
+          return resolve(downloadFile(response.headers.location, targetFile))
+        }
+
+        // save the file to disk
+        const fileWriter = fs.createWriteStream(targetFile).on("finish", () => {
+          resolve({})
+        })
+
+        response.pipe(fileWriter)
+      })
+      .on("error", (error) => {
+        reject(error)
+      })
+  })
+}
+
+await (async () => {
+  let fileText = fs.readFileSync("./data/albums.ts").toString()
+
+  for (const album of Albums) {
+    const imgUrl = album.album.image[0]["#text"]
+    const imgFileName =
+      "./assets/" + imgUrl.substring(imgUrl.lastIndexOf("/") + 1)
+    downloadFile(imgUrl, imgFileName)
+    fileText = fileText.replace(imgUrl, imgFileName)
+  }
+
+  console.log(fileText)
+})()
