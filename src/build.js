4eac2556835eec5c9120cd1bafd7a3c83424b726
Help-14 <help-14@hotmail.com>
2023-09-17 17:24:20 +0700

    temp

---
+++ src/build.js
@@ -0,0 +1,45 @@
+import { Albums } from "./data/albums"
+import * as fs from "fs"
+import https from "https"
+
+async function downloadFile(url, targetFile) {
+  return await new Promise((resolve, reject) => {
+    https
+      .get(url, (response) => {
+        const code = response.statusCode ?? 0
+
+        if (code >= 400) {
+          return reject(new Error(response.statusMessage))
+        }
+
+        // handle redirects
+        if (code > 300 && code < 400 && !!response.headers.location) {
+          return resolve(downloadFile(response.headers.location, targetFile))
+        }
+
+        // save the file to disk
+        const fileWriter = fs.createWriteStream(targetFile).on("finish", () => {
+          resolve({})
+        })
+
+        response.pipe(fileWriter)
+      })
+      .on("error", (error) => {
+        reject(error)
+      })
+  })
+}
+
+; (async () => {
+  let fileText = fs.readFileSync("./data/albums.ts").toString()
+
+  for (const album of Albums) {
+    const imgUrl = album.album.image[0]["#text"]
+    const imgFileName =
+      "./assets/" + imgUrl.substring(imgUrl.lastIndexOf("/") + 1)
+    await downloadFile(imgUrl, imgFileName)
+    fileText = fileText.replaceAll(imgUrl, imgFileName)
+  }
+
+  console.log(fileText)
+})()
